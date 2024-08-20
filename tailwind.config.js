f8eef458e807361381582cb2cbe921eba20e05c0
Sahrial Ardians <sahrialardians@gmail.com>
2024-08-20 19:48:32 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: ['index.html'],
+    theme: {
+        extends: {
+            colors: {
+                primary: '#1e3a8a',
+                dark: '#eab308',
+            }
+        },
+    },
+    plugins: [],
+};
